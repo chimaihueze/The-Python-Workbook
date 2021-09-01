8b470bf9644b2c646cb075ba7f17e2a4cd42f4c3
Chima Ihueze <kihueze@gmail.com>
2021-09-01 23:23:53 +0100

    updated commit

---
+++ Chapter-1/exercise6_tax_and_tip.py
@@ -0,0 +1,18 @@
+"""
+The program that you create for this exercise will begin by reading the cost of a meal
+ordered at a restaurant from the user. Then your program will compute the tax and
+tip for the meal. Use your local tax rate when computing the amount of tax owing.
+
+Compute the tip as 18 percent of the meal amount (without the tax). The output from
+your program should include the tax amount, the tip amount, and the grand total for
+the meal including both the tax and the tip. Format the output so that all of the values
+are displayed using two decimal places.
+"""
+
+cost_of_food = float(input("Cost of meal: "))
+tax_rate = 0.24
+tax = cost_of_food * tax_rate 
+tip = cost_of_food * 0.18 
+total = cost_of_food + tax + tip
+
+print("The tax amount is {:.2f}, the tip amount is {:.2f}, while the grand total is {:.2f}".format(tax, tip, total))
