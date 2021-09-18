38048e9b70aeaee348255a21f2b8342077c2db7e
Chima Ihueze <kihueze@gmail.com>
2021-09-18 16:57:54 +0100

    updated commit

--- Chapter-1/exercise1_mailing_address.py
+++
@@ -1,8 +0,0 @@
-"""
-Create a program that displays your name and complete mailing address. The address
-should be printed in the format that is normally used in the area where you live. Your
-program does not need to read any input from the user.
-"""
-
-print("Chima Ihueze")
-print("NAUTH, Nnewi, Anambra State")
